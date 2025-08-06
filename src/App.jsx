f6ff910f05fa6766eb2d698c6589daa3561d0f9c
Wayne <waynem567@gmail.com>
2025-08-06 14:29:42 +0200

    Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { useState, useEffect } from 'react'
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`)
-  }), [hasLiked];
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br/> {count || null}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '❤︎'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-
   return (
-    <div className="card-container">
-      <Card title="Get Shorty" rating={4} isCool={true}/>
-      <Card title="Be Cool"/>
-      <Card title="Rum Punch"/>
-    </div>
+    <div>App</div>
   )
 }
+
 export default App
