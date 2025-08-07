82c79132074a22c485d2764dfb80e803f035b9ce
Wayne <waynem567@gmail.com>
2025-08-07 09:52:13 +0200

    Define API options

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ const API_BASE_URL = 'https://api.themoviedb.org/3/';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
