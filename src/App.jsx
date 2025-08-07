0c76c750a4eae76c4a4fd8409b4b89574f83c125
Wayne <waynem567@gmail.com>
2025-08-07 15:37:52 +0200

    Add margin for section

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ const App = () => {
         </header>
 
         <section className='all-movies'>
-          <h2>All Movies</h2>
+          <h2 className='mt-[40px]'>All Movies</h2>
           
           {isLoading ? (
             <Spinner />
