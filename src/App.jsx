b44a44170791f8e1c45562367d172b3e3c22cd16
Wayne <waynem567@gmail.com>
2025-08-07 10:36:13 +0200

    Fetch movies with try/catch statement to handle errors

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,19 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}search/movie?query=${searchTerm}`, API_OPTIONS);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage(`Error fetching movies: ${error}`);
+    }
+  }
+
   useEffect(() => {
     const storedSearchTerm = localStorage.getItem('searchTerm');
     if (storedSearchTerm) {
@@ -31,9 +44,12 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find the <span className='text-gradient'>Movies</span> You'll Love Without the Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <section className='all-movies'>
+          <h2>All Movies</h2>>
+        </section>
       </div>
     </main>
   )
