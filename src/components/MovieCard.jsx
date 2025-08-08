a29f7c13fe9fa182bff98685a9f9a71258fe8ce9
Wayne <waynem567@gmail.com>
2025-08-08 08:40:15 +0200

    Movie card images

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,8 @@ const MovieCard = ({ movie:
     { title, vote_average, poster_path, release_date, original_language } 
 }) => {
   return (
-    <div>
-      <p className='text-white'>{title}</p>
+    <div className='movie-card'>
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png' } alt={title} />
     </div>
   )
 }
