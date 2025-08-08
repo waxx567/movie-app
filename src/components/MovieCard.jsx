4fe7e4b7d07fff0ce0af67ddb4757f51809d01a0
Wayne <waynem567@gmail.com>
2025-08-08 09:25:16 +0200

    Movie card content

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,21 @@ const MovieCard = ({ movie:
 }) => {
   return (
     <div className='movie-card'>
-      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png' } alt={title} />
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png' } alt={title} 
+      />
+      
+      <div className='mt-4'>
+        <h3>{title}</h3>
+
+        <div className='content'>
+          <span>{release_date}</span>
+          <span>{original_language}</span>
+          <div className='rating'>
+            <img src="./star.svg" alt="Star" />
+            <p>{vote_average}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
