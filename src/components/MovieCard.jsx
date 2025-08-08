18930a25e82acaf11e5dce9f92d4fec7b3728bb0
Wayne <waynem567@gmail.com>
2025-08-08 09:30:14 +0200

    Round star rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,11 +12,9 @@ const MovieCard = ({ movie:
         <h3>{title}</h3>
 
         <div className='content'>
-          <span>{release_date}</span>
-          <span>{original_language}</span>
           <div className='rating'>
-            <img src="./star.svg" alt="Star" />
-            <p>{vote_average}</p>
+            <img src="./star.svg" alt="Star icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
           </div>
         </div>
       </div>
