67ec8e15d0a968132a0dcc0421fb02c32ea800c8
Wayne <waynem567@gmail.com>
2025-08-08 09:56:46 +0200

    Movie card content

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -16,6 +16,17 @@ const MovieCard = ({ movie:
             <img src="./star.svg" alt="Star icon" />
             <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
           </div>
+
+          <span>•</span>
+
+          <p className='lang'>{original_language}</p> 
+          
+          <span>•</span> 
+          
+          <p className='year'> 
+            {release_date ? release_date.split('-')[0] : 'N/A'}
+          </p>
+          
         </div>
       </div>
     </div>
