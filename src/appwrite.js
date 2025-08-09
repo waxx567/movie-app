7d242f1bbef37719e66a5a84639fcc2eea215f51
Wayne <waynem567@gmail.com>
2025-08-09 14:08:30 +0200

    Save searches

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,7 +1,41 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-export const updateSearchCount = async () => {
-  console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    // 1. Use Appwrite SDK to see if the search term already exists in the database
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', searchTerm),
+        ])
+
+        // 2. If it does exist, update the count
+        if(result.documents.length > 0) {
+            const doc = result.documents[0];
+
+            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+                count: doc.count + 1
+            })
+        // 3. Otherwise create a new document with the search term and count as 1
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+            })
+        }
+    } catch (error) {
+        console.error(error);         
+    }
+    
+    
 }
