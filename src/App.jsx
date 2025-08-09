b97f2f989adc8d6bd989b6a649cc6897d0067c40
Wayne <waynem567@gmail.com>
2025-08-09 13:18:51 +0200

    Check appwrite connections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/';
 
@@ -52,6 +53,8 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+
+      updateSearchCount();
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
