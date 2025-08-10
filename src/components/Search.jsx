cb124479baad87c8c6526242038736e43acdfec5
Wayne <waynem567@gmail.com>
2025-08-10 12:53:13 +0200

    Comments

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+/**
+ * A search component that allows users to input a search term.
+ * Displays a search bar with an input field and search icon.
+ * 
+ * @param {string} searchTerm - The current value of the search input.
+ * @param {function} setSearchTerm - Function to update the search term state.
+ * 
+ * The component renders an input field with a placeholder and an onChange handler
+ * to update the search term state as the user types.
+ */
+
 const Search = ({ searchTerm, setSearchTerm }) => { 
   return (
     <div className='search'>
